9d9dbcfb730a4285bbb639c558a9f8afd730d6be
Matthijs <matthijs.booman@hotmail.com>
2014-09-07 22:15:17 +0200

    lets try this

--- src/matthbo/plugin/connecttoyt/ConnectToYT.java
+++ src/matthbo/plugin/connecttoyt/ConnectToYT.java
@@ -1,10 +1,14 @@
 package matthbo.plugin.connecttoyt;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConnectToYT extends JavaPlugin{
+	
+	public static final Object pluginMSG = ChatColor.AQUA + "[ConnectToYT] " + ChatColor.RESET;
 	
 	@Override
 	public void onEnable() {
@@ -19,6 +23,21 @@ public class ConnectToYT extends JavaPlugin{
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("youtube")){
+			if(sender instanceof Player){
+				Player player = (Player)sender;
+				if(args.toString().contains(" set ")){
+					player.sendMessage("It Works!");
+					return true;
+				}else{
+					player.sendMessage("nope!");
+					return true;
+				}
+			}else{
+				sender.sendMessage("Player Command Only!");
+				return true;
+			}
+		}
 		return false;
 	}
 
