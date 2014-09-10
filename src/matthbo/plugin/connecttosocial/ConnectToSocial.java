c5516c71e2c91668562cf95ba5c01b0bfeca28b9
Matthijs <matthijs.booman@hotmail.com>
2014-09-10 14:11:59 +0200

    final commit for release

--- src/matthbo/plugin/connecttosocial/ConnectToSocial.java
+++ src/matthbo/plugin/connecttosocial/ConnectToSocial.java
@@ -13,37 +13,32 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class ConnectToSocial extends JavaPlugin{
 	
-	public static final Object pluginMSG = ChatColor.DARK_GREEN + "[ConnectToYT] " + ChatColor.RESET;
+	public static final Object pluginMSG = ChatColor.DARK_GREEN + "[ConnectToSocial] " + ChatColor.RESET;
 	public static final Object pluginUsage = pluginMSG + "" + ChatColor.DARK_RED;
 	
 	@Override
 	public void onEnable() {
 		
-		getLogger().info("ConnectToYT has been Activated");
+		getLogger().info("ConnectToSocial has been Activated");
 		
 	}
 	
 	@Override
 	public void onDisable() {
-		getLogger().info("ConnectToYT is Disconnected from the Internet!");
+		getLogger().info("ConnectToSocial is Disconnected from the Internet!");
 	}
 	
-	public void logToFile(String url, String playerName){
-		try{
-			File dataFolder = getDataFolder();
-			if(!dataFolder.exists()) dataFolder.mkdir();
-		
-			File saveTo = new File(dataFolder, playerName + ".dat");
-			if(saveTo.exists()) saveTo.delete(); saveTo.createNewFile();
-			if(!saveTo.exists()) saveTo.createNewFile();
-			FileWriter fw = new FileWriter(saveTo, true);
-			PrintWriter pw = new PrintWriter(fw);
-			pw.println(url);
-			pw.flush();
-			pw.close();
-		}catch(IOException e){e.printStackTrace();}
+	
+	public void resetFile(String playerName){
+		File dataFolder = getDataFolder();
+		if(!dataFolder.exists()) dataFolder.mkdir();
+
+		File saveTo = new File(dataFolder, playerName + ".dat");
+		if(saveTo.exists()) saveTo.delete();
+		if(!saveTo.exists()) return;
 	}
 	
 	public void fileToMessage(CommandSender sender, String target){
@@ -55,26 +50,21 @@ public class ConnectToSocial extends JavaPlugin{
 			
 			if(list.exists()){
 				BufferedReader br = new BufferedReader(new FileReader(dataFolder + "/" + target + ".dat"));
-				String str = br.readLine();
-				sender.sendMessage(pluginMSG + target + "'s Channel: " + ChatColor.BLUE + str);
+				String str;
+				sender.sendMessage(pluginMSG + target + "'s Social Media:");
+				while((str = br.readLine()) != null){
+					sender.sendMessage(pluginMSG + "" + ChatColor.BLUE + str);
+				}
 				br.close();
 			}
 		}catch(Exception e){e.printStackTrace();}
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(cmd.getName().equalsIgnoreCase("youtube")){
+		if(cmd.getName().equalsIgnoreCase("social")){
 			if(sender instanceof Player){
 				Player player = (Player)sender;
-				if(args.length == 2 && args[0].equalsIgnoreCase("set")){
-					
-					logToFile(args[1].toLowerCase(), player.getName());
-					
-					player.sendMessage(pluginMSG + "Youtube URL is set to: " + ChatColor.BLUE + args[1]);
-					
-					return true;
-				}
-				else if(args.length == 1){
+				if(args.length == 1 && !args[0].equalsIgnoreCase("reset")){
 					Player target = player.getServer().getPlayer(args[0]);
 					if(target != null){
 						fileToMessage(player, target.getName());
@@ -83,17 +73,143 @@ public class ConnectToSocial extends JavaPlugin{
 						player.sendMessage(pluginUsage + "Player is not online or does not exist!");
 						return true;
 					}
-				}else{
-					player.sendMessage(pluginUsage + "Usage: /<command> [playername]");
-					player.sendMessage(pluginUsage + "Usage: /<command> set [url]");
+				}
+				else if(args.length == 1 && args[0].equalsIgnoreCase("reset")){
+					
+					resetFile(player.getName());
+					player.sendMessage(pluginMSG + "Your Social Media List has been reset");
+					
 					return true;
 				}
-			}else{
-				sender.sendMessage("Player Command Only!");
-				return true;
-			}
+				
+				/*Youtube*/
+				else if(args.length == 3 && args[0].equalsIgnoreCase("set") && args[1].equalsIgnoreCase("youtube")){
+					
+					YoutubeCMD(args[2], player.getName());
+					player.sendMessage(pluginMSG + "Youtube URL is set to: " + ChatColor.BLUE + args[2]);
+					
+					return true;
+				}
+				
+				/*Twitch*/
+				else if(args.length == 3 && args[0].equalsIgnoreCase("set") && args[1].equalsIgnoreCase("twitch")){
+					
+					TwitchCMD(args[2], player.getName());
+					player.sendMessage(pluginMSG + "Twitch URL is set to: " + ChatColor.BLUE + args[2]);
+					
+					return true;
+				}
+				
+				/*twitter*/
+				else if(args.length == 3 && args[0].equalsIgnoreCase("set") && args[1].equalsIgnoreCase("twitter")){
+					
+					TwitterCMD(args[2], player.getName());
+					player.sendMessage(pluginMSG + "Twitter URL is set to: " + ChatColor.BLUE + args[2]);
+					
+					return true;
+				}
+				
+				/*Facebook*/
+				else if(args.length == 3 && args[0].equalsIgnoreCase("set") && args[1].equalsIgnoreCase("facebook")){
+					
+					FacebookCMD(args[2], player.getName());
+					player.sendMessage(pluginMSG + "Facebook URL is set to: " + ChatColor.BLUE + args[2]);
+					
+					return true;
+				}
+				
+				/*Skype*/
+				else if(args.length == 3 && args[0].equalsIgnoreCase("set") && args[1].equalsIgnoreCase("skype")){
+					
+					SkypeCMD(args[2], player.getName());
+					player.sendMessage(pluginMSG + "Skype Name is set to: " + ChatColor.BLUE + args[2]);
+					
+					return true;
+				}else{
+					player.sendMessage(pluginUsage + "Usage: /<command> [playername]");
+					player.sendMessage(pluginUsage + "Usage: /<command> set [media] [url]");
+					player.sendMessage(pluginUsage + "Usage: /<command> reset");
+					return true;
+				}
+			}else sender.sendMessage(pluginUsage + "Player Command Only!"); return true;
 		}
+		
 		return false;
+	}
+	
+	public void YoutubeCMD(String url, String playerName){
+		try{
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) dataFolder.mkdir();
+		
+			File saveTo = new File(dataFolder, playerName + ".dat");
+			if(!saveTo.exists()) saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("Youtube: " + url);
+			pw.flush();
+			pw.close();
+		}catch(IOException e){e.printStackTrace();}
+	}
+	
+	public void TwitchCMD(String url, String playerName){
+		try{
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) dataFolder.mkdir();
+		
+			File saveTo = new File(dataFolder, playerName + ".dat");
+			if(!saveTo.exists()) saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("Twitch:" + url);
+			pw.flush();
+			pw.close();
+		}catch(IOException e){e.printStackTrace();}
+	}
+	
+	public void TwitterCMD(String url, String playerName){
+		try{
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) dataFolder.mkdir();
+		
+			File saveTo = new File(dataFolder, playerName + ".dat");
+			if(!saveTo.exists()) saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("Twitter: " + url);
+			pw.flush();
+			pw.close();
+		}catch(IOException e){e.printStackTrace();}
+	}
+	
+	public void FacebookCMD(String url, String playerName){
+		try{
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) dataFolder.mkdir();
+		
+			File saveTo = new File(dataFolder, playerName + ".dat");
+			if(!saveTo.exists()) saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("Facebook: " + url);
+			pw.flush();
+			pw.close();
+		}catch(IOException e){e.printStackTrace();}
+	}
+	
+	public void SkypeCMD(String url, String playerName){
+		try{
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) dataFolder.mkdir();
+		
+			File saveTo = new File(dataFolder, playerName + ".dat");
+			if(!saveTo.exists()) saveTo.createNewFile();
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("Skype: "  + url);
+			pw.flush();
+			pw.close();
+		}catch(IOException e){e.printStackTrace();}
 	}
 
 }
